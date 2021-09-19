38a59180a2715555e08a8bfaa92dbaf442e86d6a
superRaptor911 <raptor.inc2018@gmail.com>
2021-09-19 14:38:52 +0530

    Lazy loading test

--- src/pages/LinearRegression.js
+++ src/pages/LinearRegression.js
@@ -1,11 +1,19 @@
-import React from 'react';
-import LrSketch from '../components/LinearRegression/LrSketch';
+/* eslint-disable no-unused-vars */
+import React, {Suspense} from 'react';
+
+const Sketch = React.lazy(() =>
+  import('../components/LinearRegression/LrSketch'),
+);
 
 const LinearRegression = () => {
+  console.log(Sketch);
+
   return (
     <div>
-      <h2>Linear Regression</h2>
-      <LrSketch />
+      <Suspense fallback={<div>Loading...</div>}>
+        <h2>Linear Regression</h2>
+        <Sketch />
+      </Suspense>
     </div>
   );
 };
